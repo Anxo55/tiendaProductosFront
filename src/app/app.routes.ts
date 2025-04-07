2bf4f9b81639a45b73ad218316f05e1cfbc2e8d0
Anxo55 <anxo.campos260204@gmail.com>
2025-04-07 10:57:10 +0200

    first-commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+export const routes: Routes = [
+
+    {path: "", component: InicioComponent},
+    {path: "productos", component: ProductosComponent},
+    {path: "categorias", component: CategoriasComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent}
+
+];
