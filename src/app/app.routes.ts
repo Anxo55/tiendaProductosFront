b31943cd04364e57f12519907c6de262fca1fd50
Anxo55 <anxo.campos260204@gmail.com>
2025-04-08 17:59:37 +0200

    añadiendo estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CategoriasComponent } from './pages/categorias/categorias.component';
 import { Component } from '@angular/core';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 export const routes: Routes = [
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: "productos", component: ProductosComponent},
     {path: "categorias", component: CategoriasComponent},
     {path: "login", component: LoginComponent},
-    {path: "register", component: RegisterComponent}
+    {path: "register", component: RegisterComponent},
+    {path: "contacto", component: ContactoComponent}
 
 ];
